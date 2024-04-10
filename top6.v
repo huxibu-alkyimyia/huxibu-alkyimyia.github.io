376118fc08af62d53789bbcfb14202e3509c13fc
huxibu-alkyimyia <midgard2498@163.com>
2024-04-10 14:14:03 +0800

    Deployed b25b39e with MkDocs version: 1.5.3

--- top6.v
+++ top6.v
@@ -1,6 +1,12 @@
 `timescale 1ns / 1ps
 
 module top(
+    /* description of SW[9:0]:
+     * SW[3:0] : the four bits that control the number that the segments display
+     * SW[7:4] : four enables that control which ones of four segments does display numbers
+     * SW[8] : controls if the decimal point is displayed
+     * SW[9] : global enable
+     */
     input [9:0] SW,
     output [7:0] SEG,
     output [3:0] AN
